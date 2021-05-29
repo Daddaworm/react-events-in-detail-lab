fa47c7bbaea0b9c04da6d536b3bf1bf829725b89
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-05-29 18:38:48 -0400

    Completed assignment

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,22 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react' 
+
+
+export default class CoordinatesButton extends Component {
+
+    coordinatesHandler = (e) => {
+        const x = e.clientX
+        const y = e.clientY
+        const coordinates = [x, y]
+        this.props.onReceiveCoordinates(coordinates)
+    }
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.coordinatesHandler}>test</button>
+            </div>
+        )
+    }
+
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react' 
+
+
+export default class DelayedButton extends Component {
+
+    delayHandler = (e) => {
+        e.persist();
+        setTimeout(() => {return this.props.onDelayedClick(e)}, this.props.delay)
+    }
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.delayHandler}>Delayed Button</button>
+            </div>
+        )
+    }
+
+}
